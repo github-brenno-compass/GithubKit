039c01c6884145b1ddc932ad504af062dcc2fdb6
brenno-compass <135348136+brenno-compass@users.noreply.github.com>
2023-06-03 08:25:35 -0300

    feat: Added global use case for authentication API

---
+++ Sources/GithubKit/Apps/Authentication/Use Cases/GetAuthorizationTokenUseCaseProtocol.swift
@@ -0,0 +1,31 @@
+//
+//  GetAuthorizationTokenUseCase.swift
+//  
+//
+//  Created by Brenno on 03/06/23.
+//
+
+import Foundation
+import Factory
+
+/// The protocol defines a UseCase for obtaining the access token of the authenticated user.
+public protocol GetAuthorizationTokenUseCaseProtocol {
+
+    /// Method to obtain the authorization token.
+    ///
+    /// - Throws: If there is no registered token.
+    func callAsFunction() async throws -> String
+}
+
+extension Container {
+
+    /**
+     Property for obtaining the registered use case
+
+     - Warning: If the dependency has not been registered, obtaining the use case will generate a fatal
+     error.
+     */
+    public var getAuthenticationTokenUseCase: Factory<GetAPITokenUseCaseProtocol> {
+        self { fatalError() }
+    }
+}
