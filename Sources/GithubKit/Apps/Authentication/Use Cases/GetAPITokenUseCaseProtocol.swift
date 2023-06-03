839e4718bd3418a4db4df5ed235538311dcbcf98
Brenno <135348136+brenno-compass@users.noreply.github.com>
2023-06-03 18:34:22 -0300

    Add some global use case (#5)
    ## Description
    
    <!--- 
    Please include a summary of the change and which issue is fixed. Please
    also include relevant motivation and context.
    -->
    
    Implemented use cases for authenticated methods and Github API usage.
    
    ## Type of change
    
    <!--- Please delete options that are not relevant. -->
    
    - [x] New feature (non-breaking change which adds functionality)
    
    ## Checklist
    
    <!--- Please delete options that are not relevant. -->
    
    - [x] My code follows the code style of this project.
    - [x] I have updated the documentation accordingly.

---
+++ Sources/GithubKit/Apps/Authentication/Use Cases/GetAPITokenUseCaseProtocol.swift
@@ -0,0 +1,31 @@
+//
+//  GetAPITokenUseCaseProtocol.swift
+//
+//
+//  Created by Brenno on 03/06/23.
+//
+
+import Foundation
+import Factory
+
+/// The protocol defines a UseCase for obtaining the API token of the Github.
+public protocol GetAPITokenUseCaseProtocol {
+
+    /// Method to obtain the API token.
+    ///
+    /// - Throws: If there is no registered token.
+    func callAsFunction() async throws -> String
+}
+
+extension Container {
+
+    /**
+     Property for obtaining the registered use case
+
+     - Warning: If the dependency has not been registered, obtaining the use case will generate a fatal
+     error.
+     */
+    public var getAPITokenUseCase: Factory<GetAPITokenUseCaseProtocol> {
+        self { fatalError() }
+    }
+}
