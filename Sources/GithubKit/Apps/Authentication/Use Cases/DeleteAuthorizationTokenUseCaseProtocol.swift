6af3aac8a4dec8fa54ac97f39c2224f6e300cfcb
brennobemoura <37243584+brennobemoura@users.noreply.github.com>
2023-06-04 11:43:00 -0300

    Implemented Global Constants (#6)
    ## Description
    
    <!--- 
    Please include a summary of the change and which issue is fixed. Please
    also include relevant motivation and context.
    -->
    
    Implemented Global Constants to be use across the app.
    
    ## Type of change
    
    <!--- Please delete options that are not relevant. -->
    
    - [x] New feature (non-breaking change which adds functionality)
    
    ## Checklist
    
    <!--- Please delete options that are not relevant. -->
    
    - [x] My code follows the code style of this project.
    - [x] I have updated the documentation accordingly.

---
+++ Sources/GithubKit/Apps/Authentication/Use Cases/DeleteAuthorizationTokenUseCaseProtocol.swift
@@ -0,0 +1,31 @@
+//
+//  DeleteAuthorizationTokenUseCaseProtocol.swift
+//
+//
+//  Created by Brenno on 04/06/23.
+//
+
+import Foundation
+import Factory
+
+/// The protocol defines a UseCase to delete the access token of the authenticated user.
+public protocol DeleteAuthorizationTokenUseCaseProtocol {
+
+    /// Method to delete the access token.
+    ///
+    /// - Throws: If an error occurred during operation.
+    func callAsFunction() throws
+}
+
+extension Container {
+
+    /**
+     Property for obtaining the registered use case
+
+     - Warning: If the dependency has not been registered, obtaining the use case will generate a fatal
+     error.
+     */
+    public var deleteAuthorizationTokenUseCase: Factory<DeleteAuthorizationTokenUseCaseProtocol> {
+        self { fatalError() }
+    }
+}
