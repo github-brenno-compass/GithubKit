e396758b48ed151adc673bb9277b31b800b222a3
brenno-compass <135348136+brenno-compass@users.noreply.github.com>
2023-06-04 10:56:19 -0300

    feat: Implemented Global Constants

---
+++ Sources/GithubKit/Models/GlobalConstants.swift
@@ -0,0 +1,29 @@
+//
+//  GlobalConstants.swift
+//  
+//
+//  Created by Brenno on 04/06/23.
+//
+
+import Foundation
+import Factory
+
+/// This protocol defines the global constant to be use across the app.
+public protocol GlobalConstants {
+
+    /// The API token (Github Services)
+    var apiToken: String { get }
+}
+
+extension Container {
+
+    /**
+     Property for obtaining the registered constants.
+
+     - Warning: If the dependency has not been registered, obtaining the use case will generate a fatal
+     error.
+     */
+    public var globalConstants: Factory<GlobalConstants> {
+        self { fatalError() }
+    }
+}
